10ff2026cc97b0305a8860ffe34f94997332a391
keenlooks <keenlooks@gmail.com>
2017-02-08 21:39:56 -0500

    initial commit. compiles

---
+++ DRLBot.py
@@ -0,0 +1,218 @@
+import itertools as it
+import pickle
+from random import sample, randint, random
+from time import time
+import cv2
+import numpy as np
+import theano
+from lasagne.init import GlorotUniform, Constant
+from lasagne.layers import Conv2DLayer, InputLayer, DenseLayer, MaxPool2DLayer, get_output, get_all_params, \
+    get_all_param_values, set_all_param_values
+from lasagne.nonlinearities import rectify
+from lasagne.objectives import squared_error
+from lasagne.updates import rmsprop
+from theano import tensor
+from tqdm import *
+from time import sleep
+
+# Replay memory:
+class ReplayMemory:
+    def __init__(self, capacity, downsampled_x, downsampled_y):
+
+        state_shape = (capacity, 1, downsampled_y, downsampled_x)
+        self.s1 = np.zeros(state_shape, dtype=np.float32)
+        self.s2 = np.zeros(state_shape, dtype=np.float32)
+        self.a = np.zeros(capacity, dtype=np.int32)
+        self.r = np.zeros(capacity, dtype=np.float32)
+        self.nonterminal = np.zeros(capacity, dtype=np.bool_)
+
+        self.size = 0
+        self.capacity = capacity
+        self.oldest_index = 0
+
+    def add_transition(self, s1, action, s2, reward):
+        self.s1[self.oldest_index, 0] = s1
+        if s2 is None:
+            self.nonterminal[self.oldest_index] = False
+        else:
+            self.s2[self.oldest_index, 0] = s2
+            self.nonterminal[self.oldest_index] = True
+        self.a[self.oldest_index] = action
+        self.r[self.oldest_index] = reward
+
+        self.oldest_index = (self.oldest_index + 1) % self.capacity
+
+        self.size = min(self.size + 1, self.capacity)
+
+    def get_sample(self, sample_size):
+        i = sample(range(0, self.size), sample_size)
+        return self.s1[i], self.s2[i], self.a[i], self.r[i], self.nonterminal[i]
+
+# DRLBot
+class DRLBot:
+    # Q-learning settings:
+    replay_memory_size = 10000
+    discount_factor = 0.99
+    start_epsilon = float(1.0)
+    end_epsilon = float(0.1)
+    epsilon = start_epsilon
+    epsilon = 1.0
+    static_epsilon_steps = 5000
+    epsilon_decay_steps = 20000
+    epsilon_decay_stride = (start_epsilon - end_epsilon) / epsilon_decay_steps
+
+    # Max reward is about 100 (for killing) so it'll be normalized
+    reward_scale = 0.01
+
+    # Some of the network's and learning settings:
+    learning_rate = 0.00001
+    batch_size = 32
+    epochs = 20
+    training_steps_per_epoch = 5000
+    test_episodes_per_epoch = 100
+
+    # Other parameters
+    skiprate = 7
+    downsampled_x = 100
+    downsampled_y = downsampled_x
+    episodes_to_watch = 10
+
+    # Where to save and load network's weights.
+    #params_savefile = "basic_params"
+    params_savefile = "basic_params"
+    params_loadfile = "ivomi_params" #"basic_params"
+
+    ############
+    #Initialization function
+    #gets the required get_state_f and make_action_f
+    #   get_state_f
+    #   - Does not take input
+    #   - Returns current image/state or None if game finished or -1 if waiting for next frame
+    #   make_action_f
+    #   - Takes action as integer
+    #   - Returns resulting reward from action
+    #   available_actions_num
+    #   - an integer describing how many actions are available to the dqn
+    #
+    ############
+    def __init__(self,name="Darryl",get_state_f=None,make_action_f=None,available_actions_num=0):
+        self.name = name
+        self.get_state_f = get_state_f
+        self.make_action_f = make_action_f
+        self.available_actions_num = available_actions_num
+        self.memory = ReplayMemory(capacity=self.replay_memory_size, downsampled_x=self.downsampled_x, downsampled_y=self.downsampled_y)
+        self.dqn, self.learn, self.get_q_values, self.get_best_action = self.create_network(self.available_actions_num)
+        
+    def convert(self,img):
+        img = img.astype(np.float32) / 255.0
+        img = cv2.resize(img, (self.downsampled_x, self.downsampled_y))
+        return img
+
+    # Creates the network:
+    def create_network(self,available_actions_num = None):
+        if available_actions_num == None:
+            available_actions_num = self.available_actions_num
+        # Creates the input variables
+        s1 = tensor.tensor4("States")
+        a = tensor.vector("Actions", dtype="int32")
+        q2 = tensor.vector("Next State best Q-Value")
+        r = tensor.vector("Rewards")
+        nonterminal = tensor.vector("Nonterminal", dtype="int8")
+
+        # Creates the input layer of the network.
+        
+        dqn = InputLayer(shape=[None, 1, self.downsampled_y, self.downsampled_x], input_var=s1)
+        
+        
+        # Adds 3 convolutional layers, each followed by a max pooling layer.
+        dqn = Conv2DLayer(dqn, num_filters=32, filter_size=[8, 8],
+                          nonlinearity=rectify, W=GlorotUniform("relu"),
+                          b=Constant(.1))
+        dqn = MaxPool2DLayer(dqn, pool_size=[2, 2])
+        dqn = Conv2DLayer(dqn, num_filters=64, filter_size=[4, 4],
+                          nonlinearity=rectify, W=GlorotUniform("relu"),
+                          b=Constant(.1))
+        dqn = MaxPool2DLayer(dqn, pool_size=[2, 2])
+        dqn = Conv2DLayer(dqn, num_filters=64, filter_size=[3, 3],
+                          nonlinearity=rectify, W=GlorotUniform("relu"),
+                          b=Constant(.1))
+        dqn = MaxPool2DLayer(dqn, pool_size=[2, 2])
+        # Adds a single fully connected layer.
+        #dqn = DenseLayer(dqn, num_units=768, nonlinearity=rectify, W=GlorotUniform("relu"),
+        #                 b=Constant(.1))
+        dqn = DenseLayer(dqn, num_units=512, nonlinearity=rectify, W=GlorotUniform("relu"),
+                         b=Constant(.1))
+        dqn = DenseLayer(dqn, num_units=384, nonlinearity=rectify, W=GlorotUniform("relu"),
+                         b=Constant(.1))
+        dqn = DenseLayer(dqn, num_units=256, nonlinearity=rectify, W=GlorotUniform("relu"),
+                         b=Constant(.1))
+        # Adds a single fully connected layer which is the output layer.
+        # (no nonlinearity as it is for approximating an arbitrary real function)
+        dqn = DenseLayer(dqn, num_units=self.available_actions_num, nonlinearity=None)
+
+        # Theano stuff
+        q = get_output(dqn)
+        # Only q for the chosen actions is updated more or less according to following formula:
+        # target Q(s,a,t) = r + gamma * max Q(s2,_,t+1)
+        target_q = tensor.set_subtensor(q[tensor.arange(q.shape[0]), a], r + self.discount_factor * nonterminal * q2)
+        loss = squared_error(q, target_q).mean()
+
+        # Updates the parameters according to the computed gradient using rmsprop.
+        params = get_all_params(dqn, trainable=True)
+        updates = rmsprop(loss, params, self.learning_rate)
+
+        # Compiles theano functions
+        print "Compiling the network ..."
+        function_learn = theano.function([s1, q2, a, r, nonterminal], loss, updates=updates, name="learn_fn")
+        function_get_q_values = theano.function([s1], q, name="eval_fn")
+        function_get_best_action = theano.function([s1], tensor.argmax(q), name="test_fn")
+        print "Network compiled."
+
+        # Returns Theano objects for the net and functions.
+        # We wouldn't need the net anymore but it is nice to save your model.
+        return dqn, function_learn, function_get_q_values, function_get_best_action
+ 
+    # Makes an action according to epsilon greedy policy and performs a single backpropagation on the network.
+    def perform_play_step(self):
+        # Checks the state and downsamples it.
+        curr_state = self.get_state_f()
+        if curr_state is None:
+            return -1
+        s1 = self.convert(curr_state)
+
+        # With probability epsilon makes a random action.
+        if random() <= self.epsilon:
+            a = randint(0, self.available_actions_num - 1)
+        else:
+            # Chooses the best action according to the network.
+            a = self.get_best_action(s1.reshape([1, 1, self.downsampled_y, self.downsampled_x]))
+        reward = self.make_action_f(a)
+        reward *= self.reward_scale
+        curr_state = self.get_state_f()
+        if curr_state is None:
+            return -1
+        s2 = self.convert(curr_state)
+        # Remember the transition that was just experienced.
+        self.memory.add_transition(s1, a, s2, reward)
+        return None
+    
+    # Makes an action according to epsilon greedy policy and performs a single backpropagation on the network.
+    def perform_learning_step(self):
+        if self.memory.size > self.batch_size:
+            s1, s2, a, reward, nonterminal = self.memory.get_sample(self.batch_size)
+            q2 = np.max(self.get_q_values(s2), axis=1)
+            loss = self.learn(s1, q2, a, reward, nonterminal)
+        else:
+            loss = 0
+        return loss
+    
+    #gathers state-transition/reward pairs to be learned later
+    def gain_experience(self,num_steps):
+        for i in xrange(min(num_steps,self.memory.capacity)):
+            if self.perform_play_step() == -1:
+                epsilon = max(0.1,self.epsilon-0.01)
+                break
+    
+    def learn_from_experience(self,num_steps):
+        for i in xrange(num_steps):
+            self.perform_learning_step()
---
+++ deep_reinforcement_learning.py
@@ -0,0 +1,226 @@
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+import MalmoPython
+import random
+import time
+import logging
+import struct
+import socket
+import os
+import sys
+import json
+import numpy as np
+from DRLBot import DRLBot
+from time import sleep
+from tqdm import *
+
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)#DEBUG) # set to INFO if you want fewer messages
+
+# create a file handler
+#handler = logging.FileHandler('depthmaprunner.log')
+#handler.setLevel(logging.DEBUG)
+
+# create a logging format
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#handler.setFormatter(formatter)
+
+# add the handlers to the logger
+#logger.addHandler(handler)
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+    
+#----------------------------------------------------------------------------------------------------------------------------------
+
+video_width = 300
+video_height = 300
+channels = 3
+
+#################
+#INPUT - Minecraft pixels
+#RETURNS - 
+#
+#################
+def processFrame( pixels ):
+    red,green,blue,depth = pixels[0::4],pixels[1::4],pixels[2::4],pixels[3::4]
+    print np.mat((video_width,video_height),red).astype(np.float32)/255.0
+    #BLABLABLA
+
+    
+action_strings = ["move","strafe","pitch","turn","jump","crouch","attack","use"]    
+actions = [[1,0,0,0,0,0,0,0],   # 0 - move forward
+           [-1,0,0,0,0,0,0,0],  # 1 - move backward
+           [1,0,0,0,1,0,0,0],   # 2 - move forward and jump
+           [0,0,0,1,0,0,0,0],   # 3 - turn right
+           [0,0,0,-1,0,0,0,0],  # 4 - turn left
+           [0,0,0,0,0,0,1,0],   # 5 - attack
+           [0,0,0,0,0,0,0,1],   # 6 - use
+           [0,0,0,0,1,0,0,0],   # 7 - jump
+           [0,0,1,0,0,0,0,0],   # 8 - look down
+           [0,0,-1,0,0,0,0,0]]  # 9 - look up
+  
+missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    
+      <About>
+        <Summary>Survive!</Summary>
+      </About>
+      
+     <ServerSection>
+        <ServerInitialConditions>
+          <Time>
+            <StartTime>6000</StartTime>
+            <AllowPassageOfTime>false</AllowPassageOfTime>
+          </Time>
+          <Weather>clear</Weather>
+        </ServerInitialConditions>
+        <ServerHandlers>
+            <DefaultWorldGenerator/>
+            <ServerQuitFromTimeUp timeLimitMs="3000000"/>
+            <ServerQuitWhenAnyAgentFinishes />
+        </ServerHandlers>
+    </ServerSection>
+
+    <AgentSection mode="Survival">
+        <Name>Jason Bourne</Name>
+        <AgentStart>
+            <Placement x="-203.5" y="80" z="217.5"/>
+        </AgentStart>
+        <AgentHandlers>
+            <ObservationFromFullStats/>
+            <VideoProducer want_depth="true">
+                <Width>''' + str(video_width) + '''</Width>
+                <Height>''' + str(video_height) + '''</Height>
+            </VideoProducer>
+            <ContinuousMovementCommands turnSpeedDegs="90" />
+            <RewardForMissionEnd rewardForDeath="100" dimension="0">
+                 <Reward reward="100" description="death" />
+            </RewardForMissionEnd>
+        </AgentHandlers>
+    </AgentSection>
+  </Mission>'''
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+
+validate = True
+my_mission = MalmoPython.MissionSpec( missionXML, validate )
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print 'ERROR:',e
+    print agent_host.getUsage()
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print agent_host.getUsage()
+    exit(0)
+
+agent_host.setObservationsPolicy(MalmoPython.ObservationsPolicy.KEEP_ALL_OBSERVATIONS)
+agent_host.setVideoPolicy(MalmoPython.VideoPolicy.KEEP_ALL_FRAMES)
+
+if agent_host.receivedArgument("test"):
+    num_reps = 1
+else:
+    num_reps = 30000
+
+my_mission_record_spec = MalmoPython.MissionRecordSpec()
+
+where_i_have_been_before = []
+
+#function that gets agents x,y,z cooordinates from JSON observations
+def get_agent_location():
+    world_state = agent_host.peekWorldState()
+    if len(world_state.observations) < 1:
+        return None
+    obs_text = world_state.observations[0].text
+    obs = json.loads(obs_text) # most recent observation
+    logger.debug(obs)
+    if not 'XPos' in obs or not 'YPos' or not 'ZPos' in obs:
+        logger.error("Incomplete observation received: %s" % obs_text)
+        return None
+    logger.debug("State: %s (x = %.2f, z = %.2f)" % ("%d:%d" % (int(obs['XPos']), int(obs['ZPos'])), float(obs[u'XPos']), float(obs[u'ZPos'])))
+    return (float(obs['XPos']), float(obs['YPos']), float(obs['ZPos']))
+
+def is_agent_in_new_area():
+    curr_loc = get_agent_location()
+    if curr_loc is None:
+        return False
+    
+    for loc in where_i_have_been_before:
+        if np.linalg.norm(np.array(curr_loc)-np.array(loc)) < 5:
+            return False
+    where_i_have_been_before.append(curr_loc)
+    return True
+    
+def get_minecraft_frame():
+    world_state = agent_host.peekWorldState()
+    while len(world_state.video_frames) < 1 and world_state.is_mission_running:
+        logger.info("Waiting for frames...")
+        time.sleep(0.001)
+        world_state = agent_host.peekWorldState()
+    if not world_state.is_mission_running:
+        return None
+    logger.info("Got frame!")
+    pixels = agent_host.peekWorldState().video_frames[0].pixels
+    red,green,blue = pixels[0::4],pixels[1::4],pixels[2::4]
+    #pixels_averaged = [int(np.mean([re,ge,be])) for re,ge,be in zip(red,green,blue)]
+    #green.extend(blue)
+    #red.extend(green)
+    #pixels_no_depth = red
+    #return np.reshape(np.mat(pixels_no_depth,dtype=np.float32),(channels,video_width,video_height),order='C')
+    return np.reshape(np.mat(green,dtype=np.float32),(video_width,video_height),order='C')
+
+    
+def make_minecraft_action(action_i):
+    for action_tuple in zip(action_strings,actions[action_i]):
+        agent_host.sendCommand(action_tuple[0]+" "+str(action_tuple[1]))
+        logger.info("doing "+action_tuple[0]+" "+str(action_tuple[1]))
+    total_reward = 0
+    rewards = agent_host.peekWorldState().rewards
+    for reward in rewards:
+        total_reward += reward.getValue()
+    if is_agent_in_new_area():
+        total_reward+=5
+   #print "reward for this move: "+str(total_reward)
+    return total_reward
+    
+drlbot = DRLBot(get_state_f=get_minecraft_frame,make_action_f=make_minecraft_action,available_actions_num=len(actions))
+
+
+
+for _ in tqdm(xrange(1000)):
+    agent_host.startMission( my_mission, my_mission_record_spec )
+    world_state = agent_host.getWorldState()
+    while not world_state.has_mission_begun:
+        sys.stdout.write(".")
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+    print
+    my_mission.setTimeOfDay(6000, False)
+    while world_state.is_mission_running:
+        world_state = agent_host.getWorldState()
+        if world_state.is_mission_running:
+            drlbot.gain_experience(1)
+    drlbot.learn_from_experience(2000)
